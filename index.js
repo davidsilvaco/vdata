9111292b4d9f42fd9c38d0063b2e811d08f2f3fd
davidsilvaco <davidscsebas@gmail.com>
2023-06-10 21:22:16 -0500

    Muestra Inicio

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//const express = require("express")
 import express from 'express'
+import csrf from 'csurf';
+import cookieParser from 'cookie-parser';
+
 import usuarioRoutes from './routes/usuarioRoutes.js'
 import serviciosRoutes from './routes/serviciosRoutes.js';
 import appRoutes from './routes/appRoutes.js';
@@ -7,6 +9,15 @@ import db from './config/db.js';
 
 //crear el nombre de variable de la aplicacion
 const app = express()
+
+//Habilitar lectura de datos del formulario si son tipo texto email etc
+app.use( express.urlencoded({extended: true }))
+
+//Habilitar cookieParser
+app.use( cookieParser())
+
+// Habilitar CSRF
+app.use(csrf({ cookie: true}))
 
 //Conexion a la base de datos
 try {
@@ -25,20 +36,17 @@ app.set('views','./views')
 app.use(express.static('public'))
 
 
-//Rutas
-/*app.get('/', function(req,res){
-    res.send('hola muddndo')
-});*/
-
 // Routing
 app.use('/', appRoutes)
 // Use busca todo lo que inicia con una diagonal
 app.use('/auth', usuarioRoutes);
 app.use('/', serviciosRoutes);
+//app.use('/api', apiRoutes)
 
 //Definir un puero y arrancar app
-const port = 3000;
-app.listen(port,() =>{
-    console.log(`El servidor corre en ${port}`)
-});
+// Definir un puerto y arrancar el proyecto
+const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
